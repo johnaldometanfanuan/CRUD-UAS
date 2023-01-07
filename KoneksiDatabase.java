d705022597385f6e1b3aa31d4bb31a05dbf668fd
johnaldometanfanuan <118040678+johnaldometanfanuan@users.noreply.github.com>
2023-01-07 14:19:39 +0700

    Add files via upload

---
+++ KoneksiDatabase.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Koneksi;
+
+/**
+ *
+ * @author WINDOWS 11
+ */
+import java.sql.*; 
+import javax.swing.JOptionPane;
+
+public class KoneksiDatabase {
+    Connection cn; 
+    public static Connection BukaKoneksi(){
+       try {
+          Class.forName("com.mysql.jdbc.Driver");
+           Connection cn = DriverManager.getConnection ("jdbc:mysql://localhost/latihan_curd", "root","");
+           
+           return cn;
+       } catch (Exception e) {
+           JOptionPane.showMessageDialog(null,e);
+           return null;
+       }                      
+    }
+    
+}
